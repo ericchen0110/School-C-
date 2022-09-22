a3e405301e9a3694cf5d25319f89bce33d0f5fa4
U-BSD\492817 <492817@SHS-DGCSCS2.beaverton.k12.or.us>
2022-09-22 14:26:43 -0700

    first

---
+++ Student_List/Student_List.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//defin a struct
+struct Student {
+  char fist_name[];
+  char last_name[]
+  int id;
+  float gpa;
+}
+
+int main()
+{
+  
+}
