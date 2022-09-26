fa0574abff5afc2dab768352201314d857fc0fe5
U-BSD\492817 <492817@SHS-DGCSCS2.beaverton.k12.or.us>
2022-09-26 14:21:44 -0700

    not done

--- Student_List/Student_List.cpp
+++ Student_List/Student_List.cpp
@@ -1,17 +1,60 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
-//defin a struct
+//defin the struct
 struct Student {
-  char fist_name[];
-  char last_name[]
+  char *fist_name;
+  char *last_name;
   int id;
   float gpa;
-}
+};
+
+//define functions
+void add(vector<Student*> *student_vector);
+void print_fun(vector<Student*> *student_vector);
 
 int main()
+{
+  vector<Student*> student_vector;
+  
+  //get input
+  char *input;
+  cin >> input;
+  if(strcmp(input, "ADD") == 0)
+    {
+      //ADD
+      add(&student_vector);
+    }
+  else if(strcmp(input , "PRINT") == 0)
+    {
+      //PRINT
+      print_fun(&student_vector);
+    }
+}
+
+void add(vector<Student*> *student_vector)
+{
+  //define all variables
+  char first_name[21];
+  char last_name[21];
+  int id;
+  int GPA;
+
+  //get input
+  cout << "First name: ";
+  cin.get(first_name, 20);
+  cout << "\nLast name: ";
+  cin >> *last_name;
+  cout << "\nStudent ID: ";
+  cin >> id;
+  cout << "\nGPA: ";
+  cin >> GPA;
+}
+
+void print_fun(vector<Student*> *student_vector)
 {
   
 }
