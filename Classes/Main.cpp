7011fdc41c350428ba1578581a9103ddb81396d1
U-BSD\492817 <492817@SHS-DGCSCS2.beaverton.k12.or.us>
2022-11-30 14:15:40 -0800

    11/30

--- Classes/Main.cpp
+++ Classes/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 #ifndef PARENT_H
 #define PARENT_H
@@ -16,26 +17,106 @@
 #include "Music.h"
 #endif
 
+#ifndef MOVIE_H
+#define MOVIE_H
+#include "Movie.h"
+#endif
+
 using namespace std;
+
+//define functions
+void add(vector<parent*> *media_vector);
 
 int main()
 {
-  char input[3];
-  strcpy(input, "hi");
-  parent* new_p = new parent(input, 2);
-  cout << "Parent title: " << new_p->get_title() << endl;
-  cout << "Parent year: " << new_p->get_year() << endl;
+  //input
+  char input[81];
+  cout << "What do you want to do? (ADD, SEARCH, DELETE, QUIT)" << endl;
+  cin >> input;
 
-  char input1[3];
-  char input2[6];
-  strcpy(input2, "child");
-  strcpy(input1, "oh");
-  video_game* new_game = new video_game(input1, 2, input2, 3.3);
-  cout << "Video Game Title: " << new_game->get_title() << endl;
-  cout << "video game year: " << new_game->get_year() << endl;
-  cout << "video game publisher: " << new_game->get_publisher() << endl;
-  cout << "video game rating: " << new_game->get_rating() << endl;
+  //create vector
+  vector<parent*> *media_vector = new vector<parent*>();
 
-  music* new_music = new music(input, 3, input1, input2, 60);
-  cout << "music title: " << new_music->get_title() << endl;
+  //check input
+  if(strcmp(input, "ADD") == 0)
+    {
+      //ADD
+      add(media_vector);
+    }
+  else if(strcmp(input, "SEARCH") == 0)
+    {
+      //SEARCH
+    }
+  else if(strcmp(input, "DELETE") == 0)
+    {
+      //DELETE
+    }
+  else if(strcmp(input, "QUIT") == 0)
+    {
+      //QUIT
+    }
+}
+
+void add(vector<parent*> *media_vector)
+{
+  //media type
+  char type[81];
+  cout << "What type of media do you want to add? (movie, music, video_game)" << endl;
+  cin >> type;
+
+  //ask for input to each field
+  //title
+  cout << "What is the title?" << endl;
+  char title[81];
+  cin >> title;
+
+  //year
+  cout << "What is the year?" << endl;
+  int year;
+  cin >> year;
+  
+  if(strcmp(type, "movie") == 0)
+    {
+      //movie
+      //director
+      cout << "What is the name of the director?" << endl;
+      char director[81];
+      cin >> director;
+
+      //duration
+      cout << "How long is the movie in minutes?" << endl;
+      int duration;
+      cin >> duration;
+
+      //rating
+      cout << "What is the rating?" << endl;
+      float rating;
+      cin >> rating;
+    }
+  else if(strcmp(type, "video_game") == 0)
+    {
+      //video game
+      //publisher
+      cout << "What is the name of the publisher?" << endl;
+      char publisher[81];
+      cin >> publisher;
+
+      //rating
+      cout << "What is the rating?" << endl;
+      float rating;
+      cin >> rating;
+    }
+  else if(strcmp(type, "music") == 0)
+    {
+      //music
+      //artist
+      cout << "What is the name of the artist?" << endl;
+      char artist[81];
+      cin >> artist;
+
+      //duration
+      cout << "How long is the music in minutes?" << endl;
+      int duration;
+      cin >> duration;
+    }
 }
