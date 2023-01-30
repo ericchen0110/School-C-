2d4d77024b0a13172bc550ef1f4c0d68072f884a
U-BSD\492817 <492817@SHS-DGCSCS2.beaverton.k12.or.us>
2023-01-30 14:22:47 -0800

    1/30

--- HashTable/main.cpp
+++ HashTable/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cstring>
+#include <math.h>
 #include "student.h"
 #include "node.h"
 
 using namespace std;
 
 int hashFun(int id);
+int getDigit(int num, int length, int digit);
 void ADD(Node** hash_table);
 
 int main()
@@ -44,15 +46,50 @@ int main()
 int hashFun(int id)
 {
   //take the first, third, and fifth digit of the student's ID and make a three digit number. Then take the reminder of deviding the number by 100 as the hash  number
+
+  int firstD = getDigit(id, 6, 1);
+  int thirdD = getDigit(id, 6, 3);
+  int fifthD = getDigit(id, 6, 5);
   
-  int firstD = id/100000;
-  int thirdD = id/1000;
-  int fifthD = id/10;
+  cout << "firstD: " << firstD << endl;
+  cout << "thirdD: " << thirdD << endl;
+  cout << "fifthD: " << fifthD << endl;
   
   int output = firstD*100 + thirdD*10 + fifthD;
+
+  cout << "output1: " << output << endl;
   output = output%100;
+
+  cout << "output2: " << output << endl;
   
-  return output;
+  return 1;
+}
+
+int getDigit(int num, int length, int digit)
+{
+  //cout << "digit: " << digit << endl;
+  //cout << "num inside" << num << endl;
+  
+  if(digit == 1)
+    {
+      //cout << "working1\n";
+      //cout << "num orgin: " << num << endl;
+      //cout << "num: " << num/pow(10, length) << endl;
+      return num/pow(10, length-1);
+    }
+  else if(digit > 1 && length == digit)
+    {
+      int temp = num/pow(10, length-1);
+      num -= temp;
+      getDigit(num, length-1, digit-1);
+    }
+  else if(digit > 1 )
+    {
+      num = (num/pow(10, (length-digit)));
+      getDigit(num, digit, digit);
+    }
+  
+  return -1;
 }
 
 void ADD(Node** hash_table)
@@ -72,6 +109,7 @@ void ADD(Node** hash_table)
   cout << "GPA: ";
   cin >> gpa;
 
+
   //hash the id
   int hash_value = hashFun(id);
 
@@ -81,6 +119,7 @@ void ADD(Node** hash_table)
   //add to a node
   Node* new_node = new Node(new_student);
 
+  //cout << "value: " << hash_value << endl;
   //add to the hash_table array
-  hash_table[hash_value]->setNext(new_node);
+  //hash_table[hash_value]->setNext(new_node);
 }
