a69d3db0eda01e715a41447a9ec87d33f7bbb328
Junze Chen <jychenjz@gmail.com>
2023-06-04 09:13:28 +0100

    6/4

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -168,8 +168,178 @@ void Graph::removeEdge(char A, char B)
 
 void Graph::shortPath(char A, char B)
 {
-  cout << "Point A: " << A << endl;
-  cout << "Point B: " << B << endl;
+  //check if the vertices exist
+  bool existA = false;
+  bool existB = false;
+  int starting = 0;
+  
+  for(int i=0; i < charVec->size(); i++)
+    {
+      if((*charVec)[i] == A)
+	{
+	  starting = i;
+	  existA = true;
+	}
+      if((*charVec)[i] == B)
+	{
+	  existB = true;
+	}
+    }
+
+  if(!existA || !existB)
+    {
+      cout << "One of the vertices do not exist" << endl;
+      return;
+    }
+
+  //find the path
+  int vertexNum = charVec->size();
+  
+  //make the output array
+  char vertexArr[vertexNum];
+  int distance[vertexNum];
+  char previousV[vertexNum];
+
+  //initialize arrays
+  for(int i=starting; i < charVec->size(); i++)
+    {
+      vertexArr[i-starting] = (*charVec)[i];
+    }
+
+  for(int i=0; i < starting; i++)
+    {
+      vertexArr[i+starting+1] = (*charVec)[i];
+    }
+
+  distance[0] = 0;
+  previousV[0] = '\0';
+  for(int i=1; i < vertexNum; i++)
+    {
+      distance[i] = 10000;
+      previousV[i] = '\0';
+    }
+
+  //make visited and unvisited vectors
+  vector<char> *visited = new vector<char>();
+  vector<char> *unvisited = new vector<char>();
+
+  for(int i=0; i < vertexNum; i++)
+    {
+      unvisited->push_back((*charVec)[i]); 
+    }
+
+  //visit unvisted vertex with shortest distance from the start
+  int position = 0;
+  while(unvisited->size() != 0)
+    {
+      char current = smallestUnvisited(vertexArr, distance, vertexNum, visited);
+
+      int currentLocation = -1;
+      for(int i=0; i < vertexNum; i++)
+	{
+	  if((*charVec)[i] == current)
+	    {
+	      currentLocation = i;
+	      break;
+	    }
+	}
+
+      //calculate the distance from current to unvisited neighbours
+      int currentD = 0;
+      for(int i=0; i < vertexNum; i++)
+	{
+	  if((*numTable)[currentLocation][i] > 0 && unvisitedFun(current, unvisited))
+	    {
+	      currentD = distance[position] + (*numTable)[currentLocation][i];
+	      
+	      if(currentD < distance[findVerDistanceNum((*charVec)[i], vertexArr)])
+		{
+		  distance[findVerDistanceNum((*charVec)[i], vertexArr)] = currentD;
+		  previousV[findVerDistanceNum((*charVec)[i], vertexArr)] = (*charVec)[position];
+		}
+	    }
+	}
+
+      visited->push_back(vertexArr[position]);
+
+      int thisPosition = 0;
+      for(int i=0; i < unvisited->size(); i++)
+	{
+	  if((*unvisited)[i] == vertexArr[position])
+	    {
+	      thisPosition = i;
+	    }
+	}
+      unvisited->erase(unvisited->begin() + thisPosition);
+    }
+}
+
+int Graph::findVerDistanceNum(char input, char vertexArr[])
+{
+  int location = -1;
+  for(int i=0; i < charVec->size(); i++)
+    {
+      if(vertexArr[i] == input)
+	{
+	  location = i;
+	}
+    }
+
+  return location;
+}
+
+int Graph::findVerLocation(char input)
+{
+  for(int i=0; i < charVec->size(); i++)
+    {
+      if((*charVec)[i] == input)
+	{
+	  return i;
+	}
+    }
+
+  return -1;
+}
+
+bool Graph::unvisitedFun(char input, vector<char> *unvisited)
+{
+  for(int i=0; i < unvisited->size(); i++)
+    {
+      if(input == (*unvisited)[i])
+	{
+	  return false;
+	}
+    }
+
+  return true;
+}
+
+char Graph::smallestUnvisited(char vertexArr[], int distance[], int vertexNum, vector<char> *visited)
+{
+  int smallest = 100000;
+  int position = -1;
+  char returnValue = '\0';
+
+  for(int i=0; i < vertexNum; i++)
+    {
+      if(distance[i] < smallest)
+	{
+	  smallest = distance[i];
+	  position = i;
+	  returnValue = vertexArr[i];
+	}
+    }
+
+  for(int i=0; i < visited->size(); i++)
+    {
+      if((*visited)[i] == returnValue)
+	{
+	  returnValue = '\0';
+	  break;
+	}
+    }
+    
+  return returnValue;
 }
 
 void Graph::printFun()
--- Graph/Graph.h
+++ Graph/Graph.h
@@ -17,8 +17,13 @@ class Graph {
   void addEdge(char A, char B, int weight);
   void removeVertex(char input);
   void removeEdge(char A, char B);
-  void shortPath(char A, char B);
 
+  void shortPath(char A, char B);
+  char smallestUnvisited(char vertexArr[], int distance[], int vertexNum, vector<char> *visited);
+  bool unvisitedFun(char input, vector<char> *unvisited);
+  int findVerLocation(char input);
+  int findVerDistanceNum(char input, char vertexArr[]);
+  
   void printFun();
   
  private:
